c2b675ccf278711863a7931fc1efdf221e8e1233
carlospmendiola <carlos@carlospmendiola.com>
2026-06-25 17:24:59 +0200

    Añadidos textos enla pantalla de resultados según la puntuación

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -5,13 +5,13 @@ export const Resultado = ({score,handleReset}) => {
 
   const obtenerMensaje = (valor) => {
     if (valor >= 0 && valor <= 2) {
-      return <p className="insuficiente">No tienes ni idea, Nivel cultural:  Austrolopitecus poco evolucionado</p>;
+      return <p className="mensaje-final"><strong>No tienes ni idea</strong>, Nivel cultural:  Austrolopitecus poco evolucionado</p>;
     } else if (valor >= 3 && valor <= 5) {
-      return <p className="regular">Algo sabes, pero poco. Nivel: concursante de "La isla de las tentaciones"</p>;
+      return <p className="mensaje-final"><strong>Algo sabes, pero poco</strong>. Nivel: concursante de "La isla de las tentaciones"</p>;
     } else if (valor >= 6 && valor <= 8) {
-      return <p className="bueno">Eres bueno, sabes de arte. En el rosco de Pasapalabra podrías acertar la mitad, como mucho...</p>;
+      return <p className="mensaje-final"><strong>Eres bueno</strong>, sabes de arte. En el rosco de Pasapalabra acertarías la mitad, como mucho...</p>;
     } else if (valor == 9 || valor == 10) {
-      return <p className="excelente">Eres un genio, ¿qué haces perdiendo el tiempo en este juego? Haz algo de provecho para la humanidad...</p>;
+      return <p className="mensaje-final"><strong>¡Eres un genio!</strong> ¿qué haces perdiendo el tiempo con este juego? Haz algo de provecho para la humanidad...</p>;
     } else {
       return <p className="error">Puntuación no válida.</p>;
     }
@@ -24,7 +24,7 @@ export const Resultado = ({score,handleReset}) => {
         <div><img src="https://openaccess-cdn.clevelandart.org/1927.1984/1927.1984_web.jpg" alt="Dos remeros"/></div>
         <section className='txt-Finjuego'>
           <h3>¡Juego terminado!</h3>
-          <p>Has acertado <span className='destacado'>{score}</span> de 10</p>
+          <p className='puntuacion'><span className='destacado'>{score}</span> de <span className='destacado'>10</span> aciertos</p>
           <div className="contenedor-mensaje"> {obtenerMensaje(score)} </div>
         </section>
 
