d5d5d06c27ffa6914c550d021bd92b90dade4fc0
carlospmendiola <carlos@carlospmendiola.com>
2026-06-26 09:57:55 +0200

    Añadida función para que además del texto en el resultado final salga una imagen diferente según la puntuación conseguida

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -3,29 +3,27 @@ import './_resultado.scss';
 
 export const Resultado = ({score,handleReset}) => {
 
-  const obtenerMensaje = (valor) => {
-    if (valor >= 0 && valor <= 2) {
-      return <p className="mensaje-final"><strong>No tienes ni idea</strong>, Nivel cultural:  Austrolopitecus poco evolucionado</p>;
-    } else if (valor >= 3 && valor <= 5) {
-      return <p className="mensaje-final"><strong>Algo sabes, pero poco</strong>. Nivel: concursante de "La isla de las tentaciones"</p>;
-    } else if (valor >= 6 && valor <= 8) {
-      return <p className="mensaje-final"><strong>Eres bueno</strong>, sabes de arte. En el rosco de Pasapalabra acertarías la mitad, como mucho...</p>;
-    } else if (valor == 9 || valor == 10) {
-      return <p className="mensaje-final"><strong>¡Eres un genio!</strong> ¿qué haces perdiendo el tiempo con este juego? Haz algo de provecho para la humanidad...</p>;
-    } else {
-      return <p className="error">Puntuación no válida.</p>;
+  const niveles = [
+    {max:2, imagen:"https://carlospmendiola.com/downloads/Bootcamp-Slider/aciertos-02.jpg", mensaje:<><strong>No tienes ni idea</strong><br/> Nivel: Australopithecus poco evolucionado</>},
+    {max:5, imagen:"https://carlospmendiola.com/downloads/Bootcamp-Slider/aciertos-35.jpg", mensaje:<><strong>Algo sabes, pero poco</strong>.<br/> Nivel: concursante de 'La isla de las tentaciones</>},
+    {max:8, imagen:"https://carlospmendiola.com/downloads/Bootcamp-Slider/aciertos-68.jpg", mensaje:<><strong>Eres bueno</strong>, sabes de arte. <br/> Nivel: En el rosco de Pasapalabra acertarías la mitad, como mucho...</>},
+    {max:10, imagen:"https://carlospmendiola.com/downloads/Bootcamp-Slider/aciertos-910.jpg", mensaje:<><strong>¡Eres un genio!</strong> ¿qué haces perdiendo el tiempo con este juego? <br/> Nivel: Haz algo de provecho para la humanidad...</>},
+  ]
+    const obtenerNivel = (valor) => {
+      return niveles.find(n => valor <= n.max)
     }
-  };
+
+    const nivel = obtenerNivel(score)
 
 
   return (
     <>
       <section className='container-Principal'>
-        <div><img src="https://openaccess-cdn.clevelandart.org/1927.1984/1927.1984_web.jpg" alt="Dos remeros"/></div>
+        <div><img src={nivel.imagen} alt=""/></div>
         <section className='txt-Finjuego'>
           <h3>¡Juego terminado!</h3>
           <p className='puntuacion'><span className='destacado'>{score}</span> de <span className='destacado'>10</span> aciertos</p>
-          <div className="contenedor-mensaje"> {obtenerMensaje(score)} </div>
+          <div className="contenedor-mensaje"><p className="mensaje-final">{nivel.mensaje} </p></div>
         </section>
 
         <section className='zona-botones'>
